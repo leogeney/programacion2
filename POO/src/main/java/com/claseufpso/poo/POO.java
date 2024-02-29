1b64c6625f8a4f639216c65fda525b699a2c60dd
leogeney <ljgeneyl@ufpso.edu.co>
2024-02-29 14:40:31 -0500

    creacion del proyecto

---
+++ POO/src/main/java/com/claseufpso/poo/POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.claseufpso.poo;
+
+/**
+ *
+ * @author SCIS2PC07
+ */
+public class POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
